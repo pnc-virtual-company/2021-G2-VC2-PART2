a7e5112398cd3f6391f8fa95cf7dbc8637776a2f
lydenchai <lyeden999@gmail.com>
2021-12-17 08:38:22 +0700

    #7 CRUD on back-end for STUDENT is done

---
+++ back/app/Http/Controllers/StudentController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Student;
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Student::get()->all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request -> file('image')->store('public/images');
+
+        $student = new Student();
+        $student->first_name = $request->first_name;
+        $student->last_name = $request->last_name;
+        $student->class = $request->class;
+        $student->phone = $request->phone;
+        $student->gender = $request->gender;
+        $student->image =$request->file('image')->hashName();
+        $student->save();
+        return response()->json([
+            'Message' => 'Created',
+            'data' => $student,
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+         return Student::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request -> file('image')->store('public/images');
+        
+        $student = Student::findOrFail($id);
+        $student->first_name = $request->first_name;
+        $student->last_name = $request->last_name;
+        $student->class = $request->class;
+        $student->phone = $request->phone;
+        $student->gender = $request->gender;
+        $student->image =$request->file('image')->hashName();
+        $student->save();
+         return response()->json([
+            'Message' => 'Updated',
+            'data' => $student
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $isDeleted = Student::destroy($id);
+        if($isDeleted == 1){
+            return response()->json(['massage'=>'Deleted'], 200);
+        }else{
+            return response()->json(['massage'=>'Not Found'], 404);
+        }
+    }
+}
--- back/routes/api.php
+++ back/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Public Student
+Route::get('students', [StudentController::class, 'index']);
+Route::get('students/{id}', [StudentController::class, 'show']);
+
+// Private route
+// Route::group(['middleware' => ['auth:sanctum']], function(){
+
+    // Student
+    Route::post('students', [StudentController::class, 'store']);
+    Route::put('students/{id}', [StudentController::class, 'update']);
+    Route::delete('students/{id}', [StudentController::class, 'destroy']);
+// });
