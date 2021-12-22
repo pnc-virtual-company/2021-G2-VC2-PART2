86aa0311b90c48aec400a5ff623885ea48a16e0b
lydenchai <lyeden999@gmail.com>
2021-12-22 11:06:21 +0700

    #17 add form for create user

--- front/src/components/user/UserForm.vue
+++ front/src/components/user/UserForm.vue
@@ -1,50 +1,152 @@
 <template>
   <div class="">
     <v-dialog v-model="dialog" width="500">
-        <template v-slot:activator="{ on, attrs }" class="create-user-btn">
-            <v-btn color="red darken-1" dark class="mb-2" v-bind="attrs" v-on="on" bottom fab fixed right>+</v-btn>
-        </template>
-
-        <v-card>
-            <!-- <v-card-title class="text-h5 grey lighten-2"> Privacy Policy </v-card-title>
-            <v-card-text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </v-card-text>
-            <v-divider></v-divider>
-            <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="primary" text @click="dialog = false"> I accept </v-btn>
-            </v-card-actions> -->
-            
-            <div class="">
-                <form action="#">
-                    <input type="text" placeholder="Hello world"><br>
-                    <input type="text" placeholder="Hello">
-                </form>
-            </div>
-        </v-card>
+      <template v-slot:activator="{ on, attrs }" class="create-user-btn">
+        <v-btn color="red darken-1" dark class="mb-2" v-bind="attrs" v-on="on" bottom fab fixed right>+</v-btn>
+      </template>
+      <v-card>
+        <div class="">
+          <form>
+            <h2>User</h2>
+            <v-text-field
+              v-model="name"
+              :error-messages="nameErrors"
+              :counter="10"
+              label="Name"
+              required
+              @input="$v.name.$touch()"
+              @blur="$v.name.$touch()"
+            ></v-text-field>
+            <v-text-field
+              v-model="email"
+              :error-messages="emailErrors"
+              label="E-mail"
+              required
+              @input="$v.email.$touch()"
+              @blur="$v.email.$touch()"
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              :error-messages="passwordError"
+              label="Password"
+              type="password"
+              required
+              @input="$v.password.$touch()"
+              @blur="$v.password.$touch()"
+            ></v-text-field>
+            <v-select v-model="select" :items="items" :error-messages="selectErrors" label="Role" required @change="$v.select.$touch()" @blur="$v.select.$touch()"></v-select>
+            <v-select v-if="select === 'STUDENT'" v-model="studentSelected" :studentsList="studentsList" label="Select Students" required
+            ></v-select>
+            <v-file-input label="Picture" prepend-icon="mdi-camera" v-model="picture">
+            </v-file-input>
+            <v-btn class="mr-4" @click="submit"> submit </v-btn>
+            <v-btn @click="clear"> clear </v-btn>
+          </form>
+        </div>
+      </v-card>
     </v-dialog>
   </div>
 </template>
 
+
 <script>
+    import axios from '../../axios-request.js'
+    import { validationMixin } from "vuelidate";
+    import { required, maxLength, email } from "vuelidate/lib/validators";
     export default {
-        data () {
-            return {
-                dialog: false,
-            }
+    mixins: [validationMixin],
+    validations: {
+        password: { required, maxLength: maxLength(10) },
+        name: { required, maxLength: maxLength(10) },
+        email: { required, email },
+        select: { required },
+        checkbox: {
+        checked(val) {
+            return val;
         },
-    }
+        },
+    },
+
+    data: () => ({
+        studentsList: [],
+        showStudents: false,
+        dialog: false,
+        name: "",
+        email: "",
+        select: null,
+        items: ["SOCIAL AFFAIR OFFICER", "STUDENT"],
+        picture: null,
+    }),
+
+    computed: {
+        checkboxErrors() {
+        const errors = [];
+        if (!this.$v.checkbox.$dirty) return errors;
+        !this.$v.checkbox.checked && errors.push("You must agree to continue!");
+        return errors;
+        },
+        selectErrors() {
+        const errors = [];
+        if (!this.$v.select.$dirty) return errors;
+        !this.$v.select.required && errors.push("Item is required");
+        return errors;
+        },
+        nameErrors() {
+        const errors = [];
+        if (!this.$v.name.$dirty) return errors;
+        !this.$v.name.maxLength &&
+            errors.push("Name must be at most 10 characters long");
+        !this.$v.name.required && errors.push("Name is required.");
+        return errors;
+        },
+        emailErrors() {
+        const errors = [];
+        if (!this.$v.email.$dirty) return errors;
+        !this.$v.email.email && errors.push("Must be valid e-mail");
+        !this.$v.email.required && errors.push("E-mail is required");
+        return errors;
+        },
+        passwordError() {
+        const errors = [];
+        if (!this.$v.password.$dirty) return errors;
+        !this.$v.password.required && errors.push("Password is required.");
+        return errors;
+        },
+    },
+
+    methods: {
+        submit() {
+        this.$v.$touch();
+        },
+        clear() {
+        this.$v.$reset();
+        this.name = "";
+        this.email = "";
+        this.select = null;
+        this.checkbox = false;
+        },
+        // ============================== GET STUDENTS=================================
+        getAllStudent(){
+            axios.get('/students').then(res =>{
+                this.studentsList = res.data;
+            })
+        },
+    },
+    mounted() {
+        this.getAllStudent();
+    },
+    };
 </script>
 
+
 <style scoped>
-    .create-user-btn{
-        top: 85vh;
-        float: right;
-        position: fixed;
+    .create-user-btn {
+    top: 85vh;
+    float: right;
+    position: fixed;
     }
 
-    form{
-        padding: 15px;
+    form {
+    padding: 15px;
     }
 </style>
