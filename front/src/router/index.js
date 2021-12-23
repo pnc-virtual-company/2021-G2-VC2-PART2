f679e862d78200f38eb43d66b08a1da35c853122
channarypha <channary.pha@student.passerellesnumeriques.org>
2021-12-23 13:12:37 +0700

    #16 crud permission

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import PermissionView from '../components/views/PermissionView.vue'
+import PermissionForm from '../components/views/PermissionForm.vue'
+
 Vue.use(VueRouter)
 
 const routes = [
-  
+  { path: "/permission", component: PermissionView },
+  { path: "/permissionform", component: PermissionForm },
+  { path: "/", component: PermissionView }
 ]
 
 const router = new VueRouter({
