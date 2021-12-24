8fc56368ab16bbfa35b6c1def085751f90d1823c
sinysen <siny.sen@student.passerellesnumeriques.org>
2021-12-24 10:10:23 +0700

    #20 crud permission

--- back/routes/api.php
+++ back/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PermissionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('permissions', [PermissionController::class, 'index']);
+Route::get('permissions/{id}', [PermissionController::class, 'show']);
+Route::post('permissions', [PermissionController::class, 'store']);
+Route::put('permissions/{id}', [PermissionController::class, 'update']);
+Route::delete('permissions/{id}', [PermissionController::class, 'destroy']);
